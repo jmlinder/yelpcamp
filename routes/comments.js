a7f68333d1709e752bc7bf5f1e26c38a66344f5e
Joe <joemlinder@gmail.com>
2019-03-23 22:51:15 -0500

    refactoring code.

--- routes/comments.js
+++ routes/comments.js
@@ -71,14 +71,8 @@ router.delete('/:comment_id', (req, res) => {
       res.redirect('/campgrounds/' + req.params.id);
     }
   });
-  // Comment.findByIdAndRemove(req.params.comment_id, (err) => {
-  //   if (err) {
-  //     res.redirect('back');
-  //   } else {
-  //     res.redirect('/campgrounds/' + req.params.id);
-  //   }
-  // });
 });
+
 // checks if user is logged in
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
